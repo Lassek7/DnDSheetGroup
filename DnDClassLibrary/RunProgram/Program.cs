18bd599280f96b3f593e32fd08519406c63ad9e6
Unknown <Lasse54.kristensen@live.dk>
2020-04-24 12:39:05 +0200

    first try
    done

---
+++ DnDClassLibrary/RunProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
