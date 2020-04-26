6f706f3eaf623aec404f7cafb14942a16a0a4a15
kattravn <kristina_m_22@hotmail.com>
2020-04-26 14:35:00 +0200

    attributes

---
+++ DnDClassLibrary/DnDClassLibrary/CharacterAttributes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace CharacterAttributesModifiers
+{
+    class CharacterAttributes
+    {
+        public int[] Attributes { get; set; }
+        public int[] Modifiers
+        {
+            get
+            {
+                return Attributes.Select(attr => (attr - 10) / 2).ToArray();
+            }
+        }
+        public enum Fields
+        {
+            Strength,
+            Dexterity,
+            Constitution,
+            Intelligence,
+            Wisdom,
+            Charisma
+        }
+        public CharacterAttributes()
+        {
+            Attributes = new int[6];
+        }
+    }
+}
+    
+
+
+    
+
+       
+
+
