286fa7b033d646a5d3d9912231d4807c6f0cb797
Lasse Anders Kristensen <lakr17@student.aau.dk>
2020-04-27 11:36:21 +0200

    pænere nu i inv

--- DnDClassLibrary/DnDClassLibrary/Lasse/Inventory.cs
+++ DnDClassLibrary/DnDClassLibrary/Lasse/Inventory.cs
@@ -25,49 +25,13 @@ namespace DnDClassLibrary
             switch (invtype)
             {
                 case "Item":
-                    Item item = new Item();
-
-                    item.ItemName = Utillity.ReadTextInput("Please Enter item name");
-                    item.ItemType = Utillity.ReadTextInput("Please Enter item type");
-                    item.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
-                    item.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
-                    item.Description = Utillity.ReadTextInput("Please enter the description of the Item");
-
-                    InventoryList.Add(item);
+                    AddItemToList();
                     break;
                 case "Armor":
-                    Armor armor = new Armor();
-
-                    armor.ItemName = Utillity.ReadTextInput("Please Enter item name");
-                    armor.ItemType = Utillity.ReadTextInput("Please Enter item type");
-                    armor.ACFromArmor = Utillity.ReadNumericInput("what is the AC?");
-                    armor.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
-                    armor.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
-                    armor.Description = Utillity.ReadTextInput("Please enter the description of the Item");
-                    armor.ItemEquipped = Utillity.ReadBoolInput("do you want to equip it?");
-
-                    InventoryList.Add(armor);
-                  
+                    AddArmorToList();
                     break;
                 case "Weapon":
-                    Weapon weapon = new Weapon();
-
-                    weapon.ItemName = Utillity.ReadTextInput("Please Enter item name");
-                    weapon.ItemType = Utillity.ReadTextInput("Please Enter item type");
-                    weapon.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
-                    weapon.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
-                    weapon.Description = Utillity.ReadTextInput("Please enter the description of the Item");
-                    weapon.AttributeAssociation = Utillity.ReadTextInput("what attribute is it associated with?");
-                    weapon.AttackModifier = 2; // palceholder
-                    weapon.Range = Utillity.ReadTextInput("What is the range?");
-                    weapon.Damage = Utillity.ReadTextInput("How much damage does it deal?");
-                    weapon.DamageType = Utillity.ReadTextInput("What damage type does it have?");
-                    weapon.AttackBonus = 5; // placeholder
-                    weapon.ItemEquipped = Utillity.ReadBoolInput("do you want to equip it?");
-                    weapon.Proficiency = Utillity.ReadBoolInput("Do you have Proficiency in this weapon?");
-                    weapon.ProficiencyModifier = 3; // Placeholder
-                    
-                    InventoryList.Add(weapon);
+                    AddWeaponToList();
                     break;
                 default:
                     break;
@@ -104,7 +68,54 @@ namespace DnDClassLibrary
             }
         }
 
-      
+        void AddItemToList()
+        {
+            Item item = new Item();
+
+            item.ItemName = Utillity.ReadTextInput("Please Enter item name");
+            item.ItemType = Utillity.ReadTextInput("Please Enter item type");
+            item.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
+            item.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
+            item.Description = Utillity.ReadTextInput("Please enter the description of the Item");
+
+            InventoryList.Add(item);
+        }
+        void AddArmorToList()
+        {
+            Armor armor = new Armor();
+
+            armor.ItemName = Utillity.ReadTextInput("Please Enter item name");
+            armor.ItemType = Utillity.ReadTextInput("Please Enter item type");
+            armor.ACFromArmor = Utillity.ReadNumericInput("what is the AC?");
+            armor.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
+            armor.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
+            armor.Description = Utillity.ReadTextInput("Please enter the description of the Item");
+            armor.ItemEquipped = Utillity.ReadBoolInput("do you want to equip it?");
+
+            InventoryList.Add(armor);
+
+        }
+        void AddWeaponToList()
+        {
+            Weapon weapon = new Weapon();
+
+            weapon.ItemName = Utillity.ReadTextInput("Please Enter item name");
+            weapon.ItemType = Utillity.ReadTextInput("Please Enter item type");
+            weapon.AmountHeld = Utillity.ReadNumericInput("Please enter amount");
+            weapon.WeightPerItem = Utillity.ReadNumericInput("Please enter weight per item");
+            weapon.Description = Utillity.ReadTextInput("Please enter the description of the Item");
+            weapon.AttributeAssociation = Utillity.ReadTextInput("what attribute is it associated with?");
+            weapon.AttackModifier = 2; // palceholder
+            weapon.Range = Utillity.ReadTextInput("What is the range?");
+            weapon.Damage = Utillity.ReadTextInput("How much damage does it deal?");
+            weapon.DamageType = Utillity.ReadTextInput("What damage type does it have?");
+            weapon.AttackBonus = 5; // placeholder
+            weapon.ItemEquipped = Utillity.ReadBoolInput("do you want to equip it?");
+            weapon.Proficiency = Utillity.ReadBoolInput("Do you have Proficiency in this weapon?");
+            weapon.ProficiencyModifier = 3; // Placeholder
+
+            InventoryList.Add(weapon);
+        }
         //int CarryCapacityCalc()
         //{
         //    CarryCapacity = Strength * 15;
