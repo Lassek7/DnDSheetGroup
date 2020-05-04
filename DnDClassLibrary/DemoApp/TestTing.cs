2ae9a4e6101c2992b1df2152dfc0f20351bf4b6a
Rasmus <ralloskytten@gmail.com>
2020-05-04 14:30:22 +0200

    Revert "revert"
    This reverts commit 4779a756cb7d39d6d75fb35bc8fb03c260c8e1a6.

--- DnDClassLibrary/DemoApp/TestTing.cs
+++ DnDClassLibrary/DemoApp/TestTing.cs
@@ -12,29 +12,29 @@ namespace DemoApp
         static void Main(string[] args)
         {
 
-            //DnDDatabaseManagement RunInv = new DnDDatabaseManagement();
-            //RunInv.CharatorCreation();
-            //Console.ReadKey();
+            DnDDatabaseManagement RunInv = new DnDDatabaseManagement();
+            RunInv.CharatorCreation();
+            Console.ReadKey();
 
 
-            //Kristina start
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write($"{Enum.GetNames(typeof(CharacterAttributes.Fields))[i]}: ");
-                CharacterAttributes.Attributes[i] = int.Parse(Console.ReadLine());
-            }
+            ////Kristina start
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.Write($"{Enum.GetNames(typeof(CharacterAttributes.Fields))[i]}: ");
+            //    CharacterAttributes.Attributes[i] = int.Parse(Console.ReadLine());
+            //}
 
-            for (int i = 0; i < 6; i++)
-            {
-                Console.Write($"{Enum.GetNames(typeof(CharacterAttributes.Fields))[i]} modifier: ");
-                Console.WriteLine(CharacterAttributes.Modifiers[i]);
-            }
-            //Kristina slut
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.Write($"{Enum.GetNames(typeof(CharacterAttributes.Fields))[i]} modifier: ");
+            //    Console.WriteLine(CharacterAttributes.Modifiers[i]);
+            //}
+            ////Kristina slut
 
 
-            //Ahmed start
-            FunktionerTest bom = new FunktionerTest();
-            bom.Print();
+            ////Ahmed start
+            //FunktionerTest bom = new FunktionerTest();
+            //bom.Print();
             Console.ReadLine();
             //Ahmed slut
         }
